e440183bc00f912d9e25a4cc7421368a60c57729
Tim2313 <tkisuk@gmail.com>
2023-07-29 14:53:10 +0300

    [ValidCheck] Finish testtask

--- ValidCheck/src/main/java/com/ua/valid_check/Main.java
+++ ValidCheck/src/main/java/com/ua/valid_check/Main.java
@@ -1,13 +1,12 @@
 package com.ua.valid_check;
 
 
-import com.ua.valid_check.service.ProgrammResponceService;
 import com.ua.valid_check.service.RecieveBlockedIpService;
 import com.ua.valid_check.service.ValidationCheckService;
 
-import java.io.*;
-import java.nio.file.*;
-import java.util.*;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.Set;
 
 
 public class Main {
@@ -16,8 +15,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ProgrammResponceService.validationApi();
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("EnterIPaddressor 'quit' toexit");
+        String inputIp = sc.next();
+
+        while (!inputIp.equals("quit")) {
+
+            boolean isIpValid = ValidationCheckService.isIpValid(inputIp);
+
+            if (!isIpValid) {
+                System.out.println("Invalid IP address");
+            } else {
+                Set<String> blockedIps = RecieveBlockedIpService.getBlockedIps();
+                boolean isIpBlocked = blockedIps.contains(inputIp);
+                if (isIpBlocked) {
+                    System.out.println("Accessdisallowed");
+                } else {
+                    System.out.println("Accessallowed");
+                }
+            }
+            inputIp = sc.next();
+
+        }
     }
 
 }
